4f2b80372bddcbc6d71052e360f11402d8c4229c
AWS Amplify <42283162+aws-aemilia@users.noreply.github.com>
2023-11-01 12:01:15 -0700

    Update postbuild.sh

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-rm -rf ./.amplify-hosting
+BUILD_OUTPUT_DIR="${BUILD_OUTPUT_DIR:-.amplify-hosting}"
+
+rm -rf ./${BUILD_OUTPUT_DIR}
 
 node esbuild.config.js
-cp -r ./dist ./.amplify-hosting/static
+cp -r ./dist ./${BUILD_OUTPUT_DIR}/static
 
 if [[ -z "${DEPLOY_MANIFEST_OVERRIDE}" ]]; then
-  cp ./deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+  cp ./deploy-manifest.json ./${BUILD_OUTPUT_DIR}/deploy-manifest.json
 fi
